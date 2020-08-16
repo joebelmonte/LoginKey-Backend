788353dd20f8e285aa5123dc7cd7774f79fed5ab
Joseph Belmonte <joseph.belmonte@gmail.com>
2020-08-16 11:30:09 -0400

    Added endpoint to clone with updates

--- src/routers/group.js
+++ src/routers/group.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 const Group = require('../models/group')
 const auth = require('../middleware/auth')
 const router = new express.Router()
@@ -6,7 +7,13 @@ const { generateLoginKey, generateLoginKeys } = require('../utils/loginkey')
 const { agentJoinUrl, glanceClient, agentJoinUrls, glanceClients } = require('../utils/urls')
 
 
-router.post('/groups', auth, async (req, res) => {
+var corsOptions = {
+    origin: process.env.FRONTENDORIGIN,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+
+router.post('/groups', cors(corsOptions), auth, async (req, res) => {
     const group = new Group({
         ...req.body,
         owner: req.user._id
@@ -23,7 +30,7 @@ router.post('/groups', auth, async (req, res) => {
     }
 })
 
-router.get('/groups', auth, async (req, res) => {
+router.get('/groups', cors(corsOptions), auth, async (req, res) => {
     try {
         const groups = await Group.find({owner: req.user._id})
         generateLoginKeys(groups)
@@ -35,7 +42,7 @@ router.get('/groups', auth, async (req, res) => {
     }
 })
 
-router.get('/groups/:id', auth, async (req, res) => {
+router.get('/groups/:id', cors(corsOptions), auth, async (req, res) => {
     try {
         const group = await Group.findOne({_id: req.params.id, owner: req.user._id})
         if(!group) {
@@ -50,7 +57,8 @@ router.get('/groups/:id', auth, async (req, res) => {
     }
 })
 
-router.patch('/groups/:id', auth, async (req, res) => {
+// Update group
+router.patch('/groups/:id', cors(corsOptions), auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'partnerId', 'apiKey', 'partnerUserId', 'timeout']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -76,7 +84,44 @@ router.patch('/groups/:id', auth, async (req, res) => {
     }
 })
 
-router.delete('/groups/:id', auth, async (req, res) => {
+// Clone group with updates
+router.post('/groups/clone/:id', cors(corsOptions), auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'partnerId', 'apiKey', 'partnerUserId', 'timeout']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({error: "Invalid Updates!"})
+    }
+
+    try {
+        const group = await Group.findOne({_id: req.params.id, owner: req.user._id})
+        if (!group){
+            return res.status(404).send()
+        }
+
+        updates.forEach((update) => group[update] = req.body[update])
+
+        const groupClone = new Group({
+            name: group.name,
+            partnerId: group.partnerId,
+            apiKey: group.apiKey,
+            partnerUserId: group.partnerUserId,
+            timeout: group.timeout,
+            owner: req.user._id
+        })
+
+        await groupClone.save()
+        groupClone.loginKey = generateLoginKey(group)
+        groupClone.agentJoin = agentJoinUrl(group)
+        groupClone.glanceClient = glanceClient(group)
+        res.send(groupClone)
+
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/groups/:id', cors(corsOptions), auth, async (req, res) => {
     try {
         const group = await Group.findOneAndDelete({_id: req.params.id, owner: req.user._id})
         if(!group){
@@ -90,7 +135,7 @@ router.delete('/groups/:id', auth, async (req, res) => {
 })
 
 
-router.delete('/groups', auth, async (req, res) => {
+router.delete('/groups', cors(corsOptions), auth, async (req, res) => {
     try {
         const group = await Group.deleteMany({owner: req.user._id})
         if(!group){
